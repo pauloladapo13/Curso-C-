7842093c3d50864d3b493a4c60d9bcd43f39a1f6
Paul Oladapo <pauloladapo13@gmail.com>
2021-09-30 18:32:55 +0100

    Primer programa en C++

---
+++ PrimerPrograma.cpp
@@ -0,0 +1,15 @@
+//Primer programa en C++
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+//si no quieres poner "using namespace std;" en el principio del programa, escribir "std::cout<<"
+    cout<<"hola mundo :D";//si estamos usando Dev C++ , y un compilador externo tenemos que usar"<<endl;" para separar la linea que se nos aparezca.
+    
+    return 0;
+}
+
+
+
