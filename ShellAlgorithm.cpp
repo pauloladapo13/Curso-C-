acaa2af1f3d2d1280d7838861d275d22b5c16bbf
Paul <pauloladapo13@gmail.com>
2022-04-26 10:54:24 +0100

    Shell algorithm

---
+++ ShellAlgorithm.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+
+using namespace std;
+
+void intercambio(float &x, float &y){
+    float aux;
+
+    aux = x;
+    x = y;
+    y = aux; 
+}
+
+void ordenacionShell(float a[], int n){
+    int salto, i, j, k;
+    salto = n/2;
+    
+    while(salto>0){
+        for(i=salto; i<n; i++){
+            j = i - salto;
+            while(j>=0){
+                k = j + salto;
+                if(a[j] <= a[k]){
+                    j = -1;
+                    
+                    
+                }
+                else{
+                    intercambio(a[j], a[k]);
+                    j  -= salto;
+                }
+            }
+        }
+        salto = salto/2;
+    }
+}
+
+
+int main(){
+
+    float arreglo[] = {4, 6, 1, 9, 5, 10, 2, 11, 19, 7};
+
+    ordenacionShell(arreglo, 10);
+
+    cout<<"Arreglo Ordenado - Forma Ascendente"<<endl;
+    for(int i=0; i<10; i++){
+        cout<<arreglo[i]<<"|";
+
+    }
+
+    cout<<"\nArreglo Ordenado - Formas Descendente"<<endl;
+    for(int i=9; i>=0; i--){
+        cout<<arreglo[i]<<"|";
+
+    }
+
+    cout<<endl;
+
+
+         
+    
+    
+    
+    
+    
+    return 0;
+}
