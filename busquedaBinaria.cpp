089455fa84f37bd1b61f357db89ec46c34ba8554
Paul <pauloladapo13@gmail.com>
2022-05-03 22:14:22 +0100

    Busqueda Binaria

---
+++ busquedaBinaria.cpp
@@ -0,0 +1,50 @@
+// Busqueda Binaria
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    int numeros[] = {1,2,3,4,5};
+    int inf,sup, mitad, dato;
+    char band = 'F';
+
+    dato = 4; 
+
+    // Algoritmo de la busqueda Binaria
+
+    inf = 0; 
+    sup = 5;
+    
+    while(inf <= sup){
+        mitad = (inf+sup)/2;
+
+        if(numeros[mitad] == dato) {
+            band = 'V';
+            break;
+        
+        }
+        if(numeros[mitad] > dato){
+            sup = mitad;
+            mitad = (inf+ sup)/2;
+
+        }
+        if(numeros[mitad] < dato){
+            inf = mitad;
+            mitad = (inf+sup)/2;
+        }
+    
+    }
+    if(band == 'V'){
+        cout<<"El numero a sido encontrado en la posicion: "<<mitad<<endl;
+
+    }
+    else{
+        cout<<"El numero NO ha sido encontrado"<<endl;
+    }
+    
+    
+    
+    return 0;
+}
