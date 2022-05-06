cf678d1ba6ff6a881bf90c0eb07941f8f3e531a3
Paul <pauloladapo13@gmail.com>
2022-05-06 22:25:52 +0100

    Ejercicio 1 de estructuras

---
+++ ejemploEstructuras.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+struct Corredor {
+        char nombre[20];
+        int edad;
+        char sexo[10];
+        char club[20];
+    }c1;
+
+
+
+int main(){
+    char categoria[20];
+    
+    
+    cout<<"Nombre: ";
+    cin.getline(c1.nombre,20, '\n');
+    cout<<"Edad: ";
+    cin>>c1.edad;
+    fflush(stdin);
+    cout<<"Sexo: ";
+    cin.getline(c1.sexo,10, '\n');    
+    cout<<"Club: ";
+    cin.getline(c1.club, 20, '\n');
+
+    if(c1.edad <= 18) {
+        strcpy(categoria, "Juvenil");
+    }
+    else if(c1.edad <= 40) {
+        strcpy(categoria, "Senio");
+
+    }
+    else{
+        strcpy(categoria, "Veterano");
+    }
+
+    cout<<"\nDatos del corredor: "<<endl;
+    cout<<"Nombre: "<<c1.nombre<<endl;
+    cout<<"Edad: "<<c1.edad<<endl;
+    cout<<"Sexo: "<<c1.sexo<<endl;
+    cout<<"Club: "<<c1.club<<endl;
+    cout<<"Categoria: "<<categoria<<endl;
+    
+    
+    
+    
+    
+    
+    return 0;
+}
